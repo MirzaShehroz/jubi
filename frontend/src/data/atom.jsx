88a24a8a2dea32d3b332d80124830edfff010ca1
Muhammad Talha Sohail <63157276+Talha-005@users.noreply.github.com>
2021-12-20 19:43:59 +0500

    ﻿

--- frontend/src/data/atom.jsx
+++ frontend/src/data/atom.jsx
@@ -1,7 +1,7 @@
 import { atom } from "recoil";
 
 export const connectUserShow = atom({
-    key: "connect_atom",
+    key: "connectUser_atom",
     default: {
         connectMenu: false,
         csMenu: false,
