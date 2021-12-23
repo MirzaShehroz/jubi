2c20564c4117f3285d4467284e68cf6374cb7c71
47hamza <hamzazulqarnain.47@gmail.com>
2021-12-23 17:54:52 +0500

    hamza-updated

---
+++ frontend/src/components/addWatchListModal.jsx
@@ -0,0 +1,48 @@
+import {Modal,Button} from "react-bootstrap";
+import user1 from '../assets/img/user.jpg'
+
+function AddWathListModal(props)
+{
+    return(
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter" style={{color: '#3E6578'}}>
+                    Add this person to watchlist?
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-lg-4 col-sm-4'}>
+                            <img id={'modalImage'} src={user1} />
+                        </div>
+                        <div className={'col-lg-8 col-sm-8'}>
+                            <span>John Doe</span><br/>
+                            <span style={{color: '#767676'}}>52 y.o. (02/19/1966)</span>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-lg-12 col-sm-12'}>
+                            <textarea  className="form-control" id="modalTextarea" rows="3"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+
+                <Button style={{width: '150px',background: '#3E6578'}} onClick={props.onHide}>Cancel</Button>
+                <Button  style={{width: '150px',background: '#3E6578'}}>Add</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default AddWathListModal;
