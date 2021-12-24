693557b0f1cfd71b167c40355085a738c3acf1e4
47hamza <hamzazulqarnain.47@gmail.com>
2021-12-24 20:19:00 +0500

    hamza-updated

--- frontend/src/components/individualpanel.jsx
+++ frontend/src/components/individualpanel.jsx
@@ -2,6 +2,13 @@ import '../assets/css/individualpanel.css';
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import closeIcon from '../assets/img/cancelicon2.png'
+import user from "../assets/img/user.jpg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPen, faWindowClose} from "@fortawesome/free-solid-svg-icons";
+import {Button} from "react-bootstrap";
+import chatIcon from '../assets/img/chaticon.png'
+import exportIcon from '../assets/img/export.png'
+import cancelButton from '../assets/img/closeButton.png'
 
 function IndividualPanel() {
     const history = useHistory();
@@ -73,7 +80,57 @@ function IndividualPanel() {
             </div>
             <div className='individualPanel_main'>
                 <div className='individualPanel_child1'>
+                    <p className={'text-center'}><img alt="user" id={'userImg'} src={user}/></p>
+                    <br/>
+                    <h5 className={'text-center'}>John Doe</h5>
+                    <p className={'text-center'}>Female</p>
+                    <p className={'text-center'}>31 y.o. <span>(05/15/2020)</span></p>
+                    <br/>
+                    <table>
+                        <tr>
+                            <td className={'emailHead'}>Email</td>
+                            <td>hitman@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td className={'phoneHead'}>Phone</td>
+                            <td>+9256821566</td>
+                        </tr>
+                    </table>
+                    <br/>
+                    <p style={{color: '#3E6578',cursor: 'pointer'}} id={'alergies'} className={'text-center'}><u>Allergies (11) & Conditions (3)</u></p>
+                    <br/>
+                    <div id={'memo'}>
+                        <table>
+                            <tr>
+                                <td className={'memoHead'}>Memo</td>
+                                <td className={'memoHeadIcon'}><FontAwesomeIcon icon={faPen}/></td>
+                            </tr>
+                        </table>
+                        <br/>
+                        <p className={'memoMsg'}>
+                            She has heart attack family history.
+                        </p>
+                        <table>
+                            <tr>
+                                <td className={'memoHead'}>Watchlist Comments</td>
+                                <td className={'memoHeadIcon'}><u style={{color: 'red'}}>Done</u></td>
+                            </tr>
+                        </table>
+                        <div className={'container'}>
+                            <div className={'row memoWatchlist'}>
+                                <div className={'col-lg-10 col-sm-10'}>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae blanditiis error, incidunt nemo perferendis perspiciatis</p>
+                                </div>
+                                <div className={'col-lg-2 col-sm-2'}>
+                                    <img src={cancelButton} alt={'cancel'}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p className={'text-center'}><Button className={'indiButtons'} variant="outline-dark"><img src={chatIcon} alt={'chatIcon'}/> Send Message</Button></p>
+                    <p className={'text-center'}><Button className={'indiButtons'} variant="outline-dark"><img src={exportIcon} alt={'exportIcon'}/> View Detail Page</Button></p>
 
+                    <p className={'text-center'}><Button className={'indiButtons'} variant="outline-danger">Remove from Watchlist</Button></p>
                 </div>
                 <div className='individualPanel_child2'>
                     <div className='individualPanel_child21'>
