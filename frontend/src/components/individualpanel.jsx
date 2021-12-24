a191519430b89be83f6541384eb2e16b7c07e693
47hamza <hamzazulqarnain.47@gmail.com>
2021-12-24 19:25:51 +0500

    hamza-updated

--- frontend/src/components/individualpanel.jsx
+++ frontend/src/components/individualpanel.jsx
@@ -1,5 +1,5 @@
 import '../assets/css/individualpanel.css';
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import closeIcon from '../assets/img/cancelicon2.png'
 
@@ -8,17 +8,112 @@ function IndividualPanel() {
     const closeHandle = () => {
         history.push('/chat');
     }
+
+    const [btnClr1, setBtnClr1] = useState({
+        background: '#4A7389',
+        color: '#C1D6E1'
+    })
+    const [btnClr2, setBtnClr2] = useState({
+        background: '#4A7389',
+        color: '#C1D6E1'
+    })
+    const [btnClr3, setBtnClr3] = useState({
+        background: '#4A7389',
+        color: '#C1D6E1'
+    })
+
+    const changeBtnClr1 = () => {
+        setBtnClr1({
+            background: '#FFFFFF',
+            color: '#3E6578'
+        });
+        setBtnClr2({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+        setBtnClr3({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+    }
+    const changeBtnClr2 = () => {
+        setBtnClr1({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+        setBtnClr2({
+            background: '#FFFFFF',
+            color: '#3E6578'
+        });
+        setBtnClr3({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+    }
+    const changeBtnClr3 = () => {
+        setBtnClr1({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+        setBtnClr2({
+            background: '#4A7389',
+            color: '#C1D6E1'
+        });
+        setBtnClr3({
+            background: '#FFFFFF',
+            color: '#3E6578'
+        });
+    }
+
+
     return (
         <div className='individualPanel'>
             <div className='individualPanel_close'>
-                <img onClick={closeHandle} src={closeIcon} alt="something7" />
+                <img onClick={closeHandle} src={closeIcon} alt="" />
             </div>
             <div className='individualPanel_main'>
                 <div className='individualPanel_child1'>
 
                 </div>
                 <div className='individualPanel_child2'>
+                    <div className='individualPanel_child21'>
+                        <p>Period</p>
+                        <div className='dashboard_child11_1_btn'>
+                            <button onClick={changeBtnClr1} style={btnClr1} >Weekly</button>
+                            <button onClick={changeBtnClr2} style={btnClr2}>Monthly</button>
+                            <button onClick={changeBtnClr3} style={btnClr3}>Annual</button>
+                        </div>
 
+                        <div className='dashboard_child11_2_1'>
+                            <form >
+                                <select name="months" id="dashboard_month_select">
+                                    <option value="Jan">Jan</option>
+                                    <option value="Feb">Feb</option>
+                                    <option value="March">March</option>
+                                    <option value="April">April</option>
+                                    <option value="May">May</option>
+                                    <option value="June">June</option>
+                                    <option value="July">July</option>
+                                    <option value="Aug">Aug</option>
+                                    <option value="Sep">Sep</option>
+                                    <option value="Oct">Oct</option>
+                                    <option value="Nov">Nov</option>
+                                    <option value="Dec">Dec</option>
+                                </select>
+                                <select name="years" id="dashboard_year_select">
+                                    <option value={2019}>2019</option>
+                                    <option value={2020}>2020</option>
+                                    <option value={2021}>2021</option>
+                                    <option value={2022}>2022</option>
+                                    <option value={2023}>2023</option>
+                                    <option value={2024}>2024</option>
+                                    <option value={2025}>2025</option>
+                                </select>
+                                <button type='button'>G0!</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className='individualPanel_child22'></div>
                 </div>
             </div>
         </div>
