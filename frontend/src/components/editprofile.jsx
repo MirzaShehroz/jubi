0ec9f81252bf00fcf2d6fdf8bd72b5a93ca2f8cc
Muhammad Talha Sohail <63157276+Talha-005@users.noreply.github.com>
2022-01-21 18:06:42 +0500

    updates

--- frontend/src/components/editprofile.jsx
+++ frontend/src/components/editprofile.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import logo from '../assets/img/jubiwatch_logo2.png';
 import addIcon from '../assets/img/addicon.png';
 import picUpload from '../assets/img/pic_upload_icon.png';
 import { docData, userPicUpload } from '../data/atom';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function EditProfile() {
     const [avatarPreview, setAvatarPreview] = useRecoilState(userPicUpload);
-    const docData_ = useRecoilValue(docData);
+    const [docData_, setDocData] = useRecoilState(docData);
     const history = useHistory();
     const cancelButton = () => {
         history.push('/')
     }
+
+    const getData = useCallback(() => {
+        axios.get('/affiliate/v1/doctor/profile', {
+            headers: {
+                'Authorization': `Bearer ${sessionStorage.getItem('authData')}`
+            }
+        }).then(res => {
+            setDocData((obj) => ({
+                dId: res.data.data.did,
+                firstName: res.data.data.first_nmae,
+                middleName: res.data.data.middle_name,
+                lastName: res.data.data.last_name,
+                email: res.data.data.email,
+                hospital: res.data.data.hospital,
+                specialty: res.data.data.speciality,
+                title: res.data.data.title,
+                phone_number: res.data.data.phone_number,
+            }))
+        }).catch(err => console.log(err))
+    }, [setDocData])
+
+    useEffect(() => {
+        getData();
+    }, [getData])
 
     const submitSignForm = (e) => {
         e.preventDefault();
@@ -58,18 +83,30 @@ function EditProfile() {
                                 <input
                                     type="text"
                                     value={docData_.firstName}
+                                    onChange={(e) => setDocData((obj) => ({
+                                        ...obj,
+                                        firstName: e.target.value,
+                                    }))}
                                     placeholder='First name' />
                             </div>
                             <div className='signin_fields'>
                                 <input
                                     type="text"
                                     value={docData_.middleName}
+                                    onChange={(e) => setDocData((obj) => ({
+                                        ...obj,
+                                        middleName: e.target.value,
+                                    }))}
                                     placeholder='Middle name (can be empty)' />
                             </div>
                             <div className='signin_fields'>
                                 <input
                                     type="text"
                                     value={docData_.lastName}
+                                    onChange={(e) => setDocData((obj) => ({
+                                        ...obj,
+                                        lastName: e.target.value,
+                                    }))}
                                     placeholder='Last name' />
                             </div>
                             <div className='signup_email' id='editP_1'>
@@ -78,18 +115,30 @@ function EditProfile() {
                                     <input
                                         type="text"
                                         value={docData_.hospital}
+                                        onChange={(e) => setDocData((obj) => ({
+                                            ...obj,
+                                            hospital: e.target.value,
+                                        }))}
                                         placeholder='Clinic name' />
                                 </div>
                                 <div className='signin_fields'>
                                     <input
                                         type="text"
                                         value={docData_.specialty}
+                                        onChange={(e) => setDocData((obj) => ({
+                                            ...obj,
+                                            specialty: e.target.value,
+                                        }))}
                                         placeholder='Specialty or Department name' />
                                 </div>
                                 <div className='signin_fields'>
                                     <input
                                         type="text"
                                         value={docData_.title}
+                                        onChange={(e) => setDocData((obj) => ({
+                                            ...obj,
+                                            title: e.target.value,
+                                        }))}
                                         placeholder='Your title' />
                                 </div>
                             </div>
@@ -102,6 +151,10 @@ function EditProfile() {
                                     <input
                                         type="text"
                                         value={docData_.phone_number}
+                                        onChange={(e) => setDocData((obj) => ({
+                                            ...obj,
+                                            phone_number: e.target.value,
+                                        }))}
                                         placeholder='Number'
                                     />
                                 </div>
